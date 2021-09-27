b36c0572955bfb233b5c0ddbd8ad010d379ace4f
alibabashovv <90691151+alibabashovv@users.noreply.github.com>
2021-09-27 05:30:26 +0400

    Add files via upload

---
+++ enboyuk.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace enboyukeded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("en boyuk eded"+ enboyukeded(200, 234, 500, 2100, 1300, 670)) ;
+
+        }
+
+        static int enboyukeded(params int[] arry)
+        {
+            int large = 0;
+            {
+                foreach (int item in arry)
+                {
+
+                    if (item > large)
+                    {
+                        large = item;
+                    }
+                }
+                return large;
+            }
+        }
+    }
+}
+
+	
+
+
+            
