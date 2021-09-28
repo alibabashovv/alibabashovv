04cc950cf1b1499e4aafd3521a65d431b16eb817
alibabashovv <90691151+alibabashovv@users.noreply.github.com>
2021-09-28 12:46:56 +0400

    Add files via upload

---
+++ ters cevirme2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = "Hello world";
+            Customreverse(word1);
+
+        }
+
+
+        static void Customreverse(string str)
+        {
+            string[] array = str.Split(" ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                char[] word1 = array[i].ToCharArray();
+                string word2 = string.Empty;
+
+                for (int b = word1.Length - 1; b >= 0; b--)
+                {
+                    word2 += word1[b];
+                }
+                Console.WriteLine(word2);
+            }
+        }
+    }
+}
+    
