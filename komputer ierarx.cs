1005d46278cfafed64ba05dd8a8298d51b0cfe15
alibabashovv <90691151+alibabashovv@users.noreply.github.com>
2021-09-30 00:42:09 +0400

    Add files via upload

---
+++ komputer ierarx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace computer_hier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer1 = new Computer("Lenovo","Legion",5);
+            
+            Console.WriteLine(computer1.Putallname());
+            Computer functions = new Computer("Windows-10",512,8, "Nvidia-Geforce","Ryzen");
+            
+            Console.WriteLine(" ");
+            Console.WriteLine(functions.Putallfunctions());
+
+
+
+        }
+    }
+}   
+        
